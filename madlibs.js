9e8ce7dea33c5ce4d246975c9033d7436d14aa50
Carterm06 <25matthcart@washk12.org>
2025-02-27 11:36:13 -0700

    madlibs

--- madlibs.js
+++ madlibs.js
@@ -1,7 +1,7 @@
 const prompt = require("prompt-sync")(); // Ask for user inputs
 
 
-let gameCharacter = prompt("Enter a game character: ");
+let gameCharacter = prompt("Enter a name: ");
 let weapon = prompt("Enter a weapon: ");
 let whereareyou = prompt("where are you at: ");
 let going = prompt("where are you going: ");
@@ -9,5 +9,5 @@ let doing = prompt("what are you doing there: ");
 let why = prompt(" why ");
 let go = prompt(" how did it go ");
 let end = prompt(" how does your story end ");
-let story = "Once upon a time " + gameCharacter + " was on a mission to ..."
+let story = "Once upon a time " + gameCharacter + " was on a mission to ..." + whereareyou + " he was there to" + doing + " why was he doing this" + why + "using his" + weapon + go + end
 console.log(story);
